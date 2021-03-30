8924c7f5e8f163817794e6053a471db057a8ce99
haniissa <40047556+haniissa@users.noreply.github.com>
2021-03-30 12:02:18 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,16 @@ module.exports = {
   theme: {
     extend: {
     	backgroundImage: theme => ({
-        'hero-header': "url('../../images/desktop/image-hero.jpg')",
-        "interactive-vr":"url('../../images/desktop/image-interactive.jpg')",
-        "curiosity":"url('../../images/desktop/image-curiosity.jpg')",
-        "deep-earth":"url('../../images/desktop/image-deep-earth.jpg')",
-        "fisheye":"url('../../images/desktop/image-fisheye.jpg')",
-        "from-above":"url('../../images/desktop/image-from-above.jpg')",
-        "night-arcade":"url('../../images/desktop/image-night-arcade.jpg')",
-        "pocket-borealis":"url('../../images/desktop/image-pocket-borealis.jpg')",
-        "soccer":"url('../../images/desktop/image-soccer-team.jpg')",
-        "grid":"url('../../images/desktop/image-grid.jpg')",
+        'hero-header': "url('images/desktop/image-hero.jpg')",
+        "interactive-vr":"url('images/desktop/image-interactive.jpg')",
+        "curiosity":"url('images/desktop/image-curiosity.jpg')",
+        "deep-earth":"url('images/desktop/image-deep-earth.jpg')",
+        "fisheye":"url('images/desktop/image-fisheye.jpg')",
+        "from-above":"url('images/desktop/image-from-above.jpg')",
+        "night-arcade":"url('images/desktop/image-night-arcade.jpg')",
+        "pocket-borealis":"url('images/desktop/image-pocket-borealis.jpg')",
+        "soccer":"url('images/desktop/image-soccer-team.jpg')",
+        "grid":"url('images/desktop/image-grid.jpg')",
         }),
         fontFamily: {
         'body': ['Alata'],
