df9c8ab5243214cf4d6579b8ae207eaa0b11723e
haniissa <40047556+haniissa@users.noreply.github.com>
2021-03-30 11:58:26 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -48,25 +48,25 @@ function changeImage(){
 	let parentImage = document.querySelector(".grand-Image .parent-Image ")
 	let allImageSrc = [];
 	// console.log(div.firstElementChild);
-	//Chnage image 
+	//Chnage image
 	let images = [];
-	images[0] = "../../images/desktop/image-deep-earth.jpg";
-	images[1] = "../../images/desktop/image-night-arcade.jpg";
-	images[2] = "../../images/desktop/image-soccer-team.jpg";
-	images[3] = "../../images/desktop/image-grid.jpg";
-	images[4] = "../../images/desktop/image-from-above.jpg";
-	images[5] = "../../images/desktop/image-pocket-borealis.jpg";
-	images[6] = "../../images/desktop/image-curiosity.jpg";
-	images[7] = "../../images/desktop/image-fisheye.jpg";
+	images[0] = "/images/desktop/image-deep-earth.jpg";
+	images[1] = "/images/desktop/image-night-arcade.jpg";
+	images[2] = "/images/desktop/image-soccer-team.jpg";
+	images[3] = "/images/desktop/image-grid.jpg";
+	images[4] = "/images/desktop/image-from-above.jpg";
+	images[5] = "/images/desktop/image-pocket-borealis.jpg";
+	images[6] = "/images/desktop/image-curiosity.jpg";
+	images[7] = "/images/desktop/image-fisheye.jpg";
 	let imagesMobile = [];
-	imagesMobile[0] = "../../images/mobile/image-deep-earth.jpg";
-	imagesMobile[1] = "../../images/mobile/image-night-arcade.jpg";
-	imagesMobile[2] = "../../images/mobile/image-soccer-team.jpg";
-	imagesMobile[3] = "../../images/mobile/image-grid.jpg";
-	imagesMobile[4] = "../../images/mobile/image-from-above.jpg";
-	imagesMobile[5] = "../../images/mobile/image-pocket-borealis.jpg";
-	imagesMobile[6] = "../../images/mobile/image-curiosity.jpg";
-	imagesMobile[7] = "../../images/mobile/image-fisheye.jpg";
+	imagesMobile[0] = "/images/mobile/image-deep-earth.jpg";
+	imagesMobile[1] = "/images/mobile/image-night-arcade.jpg";
+	imagesMobile[2] = "/images/mobile/image-soccer-team.jpg";
+	imagesMobile[3] = "/images/mobile/image-grid.jpg";
+	imagesMobile[4] = "/images/mobile/image-from-above.jpg";
+	imagesMobile[5] = "/images/mobile/image-pocket-borealis.jpg";
+	imagesMobile[6] = "/images/mobile/image-curiosity.jpg";
+	imagesMobile[7] = "/images/mobile/image-fisheye.jpg";
 	for (let i = 0; i < parentImage.childElementCount; i++) {
 		  let s = parentImage.children[i].firstElementChild
 			// console.log(s)
@@ -81,7 +81,7 @@ function changeImage(){
 				allImageSrc[i].src = images[i];
 			else
 				allImageSrc[i].src = imagesMobile[i];
-			 
+
 	}
 
 }
@@ -91,8 +91,8 @@ changeImage()
 
 
 function show(){
-	let upBtn = document.querySelector(".our-creation .up-btn") 
-	let downBtn = document.querySelector(".our-creation .down-btn") 
+	let upBtn = document.querySelector(".our-creation .up-btn")
+	let downBtn = document.querySelector(".our-creation .down-btn")
 	window.addEventListener('resize', ()=>{
 			if (window.innerWidth > 1023) {
 				upBtn.classList.remove("hidden");
@@ -100,7 +100,7 @@ function show(){
 				downBtn.classList.add("hidden")
 				// alert("message?: DOMString");
 			}else{
-				
+
 				upBtn.classList.add("hidden");
 				upBtn.classList.add("invisible");
 				downBtn.classList.remove("hidden");
